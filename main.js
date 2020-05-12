e75e7c8033daa744210bb4faf3b44366b0660405
alitemel89 <alitemel89.ce@gmail.com>
2020-05-12 17:57:46 +0300

    update functions
    make anonymous functionsi add last lyric to nextThree function

--- main.js
+++ main.js
@@ -6,15 +6,20 @@ keys.forEach(function(key){
 })
 
 // Write named functions that change the color of the keys below
-const keyPlay = (event) => {
+const keyPlay = function(event) {
   event.target.style.backgroundColor = 'purple'
 }
 
-const keyReturn = (event) => {
+const keyReturn = function(event) {
   event.target.style.backgroundColor = ''
 }
 
-const eventAssignment = (note) => {
+
+
+
+
+// Write a named function with event handler properties
+const eventAssignment = function(note) {
   note.onmousedown = function() {
     keyPlay(event);
   }
@@ -24,15 +29,8 @@ const eventAssignment = (note) => {
 
 };
 
+
 notes.forEach(eventAssignment);
-
-
-
-// Write a named function with event handler properties
-nextOne.onclick = function() {
-  nextTwo.hidden = false;
-  nextOne.hidden = true;
-}
 // Write a loop that runs the array elements through the function
 
 
@@ -56,12 +54,12 @@ startOver.hidden= true;
 
 // Write anonymous event handler property and function for the first progress button
 nextOne.onclick = function () {
-    nextTwo.hidden = true;
+    nextTwo.hidden = false;
     nextOne.hidden= true;
 
     document.getElementById('letter-note-five').innerHTML = 'D'
     document.getElementById('letter-note-six').innerHTML = 'C'
-}
+};
 
 
 
@@ -72,18 +70,44 @@ nextTwo.onclick = function () {
     nextTwo.hidden= true;
 
     document.getElementById('word-five').innerHTML = 'DEAR'
-    document.getElementById('word-six').innerHTML = 'FRI-'
+    document.getElementById('word-six').innerHTML = 'AY-'
 
-    lastlyrics.style.display='inline-block'
-}
+    document.getElementById('letter-note-three').innerHTML = 'G'
+    document.getElementById('letter-note-four').innerHTML = 'E'
+    document.getElementById('letter-note-five').innerHTML = 'C'
+    document.getElementById('letter-note-six').innerHTML = 'B'
+
+    lastLyric.style.display ='inline-block'
+};
 // Write anonymous event handler property and function for the third progress button
+nextThree.onclick = function () {
+  startOver.hidden = false;
+  nextThree.hidden= true;
 
+  document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('word-three').innerHTML = 'BIRTH-';
+  document.getElementById('word-four').innerHTML = 'DAY';;
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('word-six').innerHTML = 'YOU!';
+
+  document.getElementById('letter-note-one').innerHTML = 'F'
+  document.getElementById('letter-note-two').innerHTML = 'F'
+  document.getElementById('letter-note-three').innerHTML = 'E'
+  document.getElementById('letter-note-four').innerHTML = 'C'
+  document.getElementById('letter-note-five').innerHTML = 'D'
+  document.getElementById('letter-note-six').innerHTML = 'C'
+  
+  lastLyric.style.display ='none';
+
+};
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function() {
   nextOne.hidden = false;
   startOver.hidden = true;
-   document.getElementById('word-one').innerHTML = 'HAP-';
+
+  document.getElementById('word-one').innerHTML = 'HAP-';
   document.getElementById('letter-note-one').innerHTML = 'G';
   document.getElementById('word-two').innerHTML = 'PY';
   document.getElementById('letter-note-two').innerHTML = 'G';
@@ -95,4 +119,7 @@ startOver.onclick = function() {
   document.getElementById('letter-note-five').innerHTML = 'C';
   document.getElementById('word-six').innerHTML = 'YOU!';
   document.getElementById('letter-note-six').innerHTML = 'B';
-}
+
+
+ 
+};
